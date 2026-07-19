af2f803ac9b1946f6faa3686fae8c275f7175e77
Paulozero99 <opaulohenrique363@gmail.com>
2026-07-18 22:44:38 -0400

    retirando variaveis desnecessaria das configurações do jogo

--- configuracoes.py
+++ configuracoes.py
@@ -1,14 +1,16 @@
 LARGURA, ALTURA = 320, 180
 
 ESCALA = 4
-LARGURA_JANELA, ALTURA_JANELA = LARGURA * 4, ALTURA * 4
+LARGURA_JANELA, ALTURA_JANELA = LARGURA * ESCALA, ALTURA * ESCALA
 
 TAMANHO = 8
 
-limite_linhas = ALTURA // 4
-limite_culunas = LARGURA // 4
+limite_linhas = ALTURA // TAMANHO
+limite_culunas = LARGURA // TAMANHO
 
-bloco = (0, 0)
-
-colidiu_horizontal = False
-colidiu_vertical = False
+bloco = [
+    (10, 0),
+    (11, 0),
+    (12, 1),
+    (11, 1)
+]
